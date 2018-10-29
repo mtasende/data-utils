45acdd510f926b2b15a9d327af260523af193862
Miguel Tasende <miguel.tasende@gmail.com>
2018-10-29 14:13:53 -0300

    added author

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(name='data_utils',
       version='0.1',
       description='Utility functions for pandas and Data Analysis.',
       packages=['data_utils'],
+      author='Miguel Tasende',
       zip_safe=False)
